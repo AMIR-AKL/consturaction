3452feabb2f5311eadc5edd1b7001b7f054c351f
amirgoody <amirhosynakhlaghian@gmail.com>
2025-09-07 08:42:37 +0330

    refactor services component

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -4,21 +4,34 @@ import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import BoxServices from "../BoxServices/BoxServices";
 import TitleElementor from "../TitleElementor/TitleElementor";
 
-export default function Services() {
+type PropsServices = {
+	linkItem: boolean;
+	data: {
+		id: number;
+		title: string;
+		text: string;
+		src: string;
+	}[];
+};
+
+export default function Services({ linkItem, data }: PropsServices) {
 	return (
-		<section dir="rtl" className="mt-10 py-5 container">
+		<section dir="rtl" className="mt-20 py-5 container">
 			<TitleElementor
 				headtitle="services"
 				maintitle="خدمات گروه مهندسی ما"
 				bottomtitle="با هدف جلب رضایت مشتریان"
 			/>
 			<div className="grid md:grid-cols-3 mt-10">
-				<BoxServices
-					title="ساخت آپارتمان"
-					text="ما با استفاده از تکنولوژی های نوین و به روز، به طور مداوم در جستجوی روش های بهبود یافته است."
-					src="https://a.meharat.com/constructionco-ml/wp-content/uploads/2023/08/iconscout-19.svg"
-				/>
-				<BoxServices
+				{data.map((box) => (
+					<BoxServices
+						key={box.id}
+						title={box.title}
+						text={box.text}
+						src={box.src}
+					/>
+				))}
+				{/* <BoxServices
 					title="ساخت آپارتمان"
 					text="ما با استفاده از تکنولوژی های نوین و به روز، به طور مداوم در جستجوی روش های بهبود یافته است."
 					src="https://a.meharat.com/constructionco-ml/wp-content/uploads/2023/08/Vector-1.svg"
@@ -37,18 +50,20 @@ export default function Services() {
 					title="ساخت آپارتمان"
 					text="ما با استفاده از تکنولوژی های نوین و به روز، به طور مداوم در جستجوی روش های بهبود یافته است."
 					src="https://a.meharat.com/constructionco-ml/wp-content/uploads/2023/08/iconscout-19.svg"
-				/>
-				<div className="flex items-center justify-center font-dana-bold max-md:mt-5">
-					<Link
-						href={"/"}
-						className="tracking-tighter flex items-center gap-x-2 group hover:gap-x-3 transition-all"
-					>
-						مشاهده همه خدمات
-						<div className="size-6 bg-amber-400 flex items-center justify-center rounded-full group-hover:rotate-360 transition-all duration-1000">
-							<MdKeyboardDoubleArrowLeft />
-						</div>
-					</Link>
-				</div>
+				/> */}
+				{linkItem && (
+					<div className="flex items-center justify-center font-dana-bold max-md:mt-5">
+						<Link
+							href={"/"}
+							className="tracking-tighter flex items-center gap-x-2 group hover:gap-x-3 transition-all"
+						>
+							مشاهده همه خدمات
+							<div className="size-6 bg-amber-400 flex items-center justify-center rounded-full group-hover:rotate-360 transition-all duration-1000">
+								<MdKeyboardDoubleArrowLeft />
+							</div>
+						</Link>
+					</div>
+				)}
 			</div>
 		</section>
 	);
